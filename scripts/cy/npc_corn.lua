bb94ecd7db41bde8ab5aae9245ea6cdb25264b39
patman <27874300+patmanf@users.noreply.github.com>
2018-07-16 12:24:00 -0500

    add  gun that makes you corn

--- scripts/cy/npc_corn.lua
+++ scripts/cy/npc_corn.lua
@@ -2,18 +2,26 @@
 local og_init = init
 
 local REPLACE_CLOTHES = {
-	headCosmetic = "cornhead",
-	chestCosmetic = "cornchest",
-	legsCosmetic = "cornlegs"
+    headCosmetic = "cornhead",
+    chestCosmetic = "cornchest",
+    legsCosmetic = "cornlegs",
+    primary = "corn",
+    alt = "corn",
+
+    back = "",
+    backCosmetic = "",
+    head = "",
+    chest = "",
+    legs = ""
 }
 
 function init()
-	og_init()
-	message.setHandler("makeCorn", makeCorn)
+    og_init()
+    message.setHandler("makeCorn", makeCorn)
 end
 
 function makeCorn()
-	for k,c in pairs(REPLACE_CLOTHES) do
-		npc.setItemSlot(k, c)
-	end
+    for k,c in pairs(REPLACE_CLOTHES) do
+        npc.setItemSlot(k, c)
+    end
 end
